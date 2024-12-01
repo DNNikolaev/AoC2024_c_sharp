8d27215eeb8040d6602f877e6b538e9b33411250
Dmitrii Nikolaev <ndn89@yandex.ru>
2024-12-01 12:49:31 +0100

    AoC 2024 day 1

---
+++ AoC2024_1/Program.cs
@@ -0,0 +1,17 @@
+﻿var streamReader = new StreamReader(@"..\..\..\input");
+
+var lines = streamReader.ReadToEnd().Split("\r\n");
+var leftList = lines.Select(l => int.Parse(l.Split(' ')[0])).ToList();
+var rightList = lines.Select(l => int.Parse(l.Split(' ')[^1])).ToList();
+
+leftList.Sort();
+rightList.Sort();
+
+var sum = leftList.Select((l, i) => Math.Abs(l - rightList[i])).Sum();
+
+Console.WriteLine($"Left: {leftList[0]}; Right: {rightList[0]} ");
+Console.WriteLine($"Sum: {sum}");
+
+var stageTwoSum = leftList.Select(l => rightList.FindAll(r => r == l).Count * l).Sum();
+
+Console.WriteLine($"Stage Two Sum: {stageTwoSum}");
