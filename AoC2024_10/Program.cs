262630f1c81906034cf4d75a467c46d4113a1001
Dmitrii Nikolaev <ndn89@yandex.ru>
2024-12-11 19:19:33 +0100

    AoC Day 11

--- AoC2024_10/Program.cs
+++ AoC2024_10/Program.cs
@@ -8,7 +8,7 @@ for (int i = 0; i < map.Length; i++)
     for (int j = 0; j < map[i].Length; j++)
     {
         if (map[i][j] != '0') continue;
-        reachablePeaksByStartingPoint.Add($"{i},{j}", getReachablePeaks(i, j, 0).ToList());
+        reachablePeaksByStartingPoint.Add($"{i},{j}", GetReachablePeaks(i, j, 0).ToList());
     }
 }
 
@@ -17,14 +17,14 @@ Console.WriteLine(reachablePeaksByStartingPoint.Select(e => e.Value.Count).Sum()
 
 return;
     
-List<string> getReachablePeaks(int x, int y, int currentHeight)
+List<string> GetReachablePeaks(int x, int y, int currentHeight)
 {
     if (currentHeight == 9) return [$"{x},{y}"];
     List<string> result = [];
-    if (x - 1 >= 0 && Char.GetNumericValue(map[x - 1][y]) == currentHeight + 1) result.AddRange(getReachablePeaks(x-1,y,currentHeight+1));
-    if (x + 1 < map.Length && Char.GetNumericValue(map[x + 1][y]) == currentHeight + 1) result.AddRange(getReachablePeaks(x+1,y,currentHeight+1));
-    if (y - 1 >= 0 && Char.GetNumericValue(map[x][y - 1]) == currentHeight + 1) result.AddRange(getReachablePeaks(x,y-1,currentHeight+1));
-    if (y + 1 < map[x].Length && Char.GetNumericValue(map[x][y + 1]) == currentHeight + 1) result.AddRange(getReachablePeaks(x,y+1,currentHeight+1));
+    if (x - 1 >= 0 && Char.GetNumericValue(map[x - 1][y]) == currentHeight + 1) result.AddRange(GetReachablePeaks(x-1,y,currentHeight+1));
+    if (x + 1 < map.Length && Char.GetNumericValue(map[x + 1][y]) == currentHeight + 1) result.AddRange(GetReachablePeaks(x+1,y,currentHeight+1));
+    if (y - 1 >= 0 && Char.GetNumericValue(map[x][y - 1]) == currentHeight + 1) result.AddRange(GetReachablePeaks(x,y-1,currentHeight+1));
+    if (y + 1 < map[x].Length && Char.GetNumericValue(map[x][y + 1]) == currentHeight + 1) result.AddRange(GetReachablePeaks(x,y+1,currentHeight+1));
 
     return result;
 }
---
+++ AoC2024_11/Program.cs
@@ -0,0 +1,40 @@
+﻿var streamReader = new StreamReader(@"..\..\..\input");
+
+var initialArrangement = streamReader.ReadToEnd();
+var initialArrangementStageTwo = initialArrangement;
+
+var cache = new Dictionary<string, long>();
+
+Console.WriteLine(initialArrangement.Split(" ").Select(v => Blink(v, 25)).Sum());
+Console.WriteLine(initialArrangementStageTwo.Split(" ").Select(v => Blink(v, 75)).Sum());
+return;
+
+
+long Blink(string input, int depth)
+{
+    var key = $"{input};{depth}";
+    var newDepth = depth - 1;
+    if (cache.TryGetValue(key, out var result)) return result;
+    if (depth == 0)
+    {
+        result = 1;
+    }
+    else if (input == "0") 
+    {
+        result = Blink("1", newDepth);
+    }
+    else if (input.Length % 2 == 1)
+    {
+        result = Blink((Int64.Parse(input) * 2024).ToString(), newDepth);
+    }
+    else
+    {
+        var middlePoint = input.Length / 2;
+        var delimitedInput = input.Insert(middlePoint, " ");
+        var values = delimitedInput.Split(" ").Select(v => Int64.Parse(v).ToString()).ToArray();
+        result = Blink(values[0], newDepth) + Blink(values[1], newDepth);
+    }
+
+    cache.Add(key, result);
+    return result; 
+}
